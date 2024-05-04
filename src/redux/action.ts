dc0e8f3e94f18c05e9baf6ba2c1c61313b364f5f
sanchitloomba123 <sanchitloomba123@gmail.com>
2024-05-04 20:29:16 +0530

    Add files via upload

---
+++ src/redux/action.ts
@@ -0,0 +1,27 @@
+// action.js
+import { ADD_CONTACT, EDIT_CONTACT, REMOVE_CONTACT } from './actionTypes';
+
+
+export const addContact = (payload: any) => {
+  console.log(payload)
+  return {
+    type: ADD_CONTACT,
+    payload,
+  };
+};
+
+export const removeContact = (id: any) => {
+  return {
+    type: REMOVE_CONTACT,
+    payload: {
+      id,
+    },
+  };
+};
+export const editContact = (payload: any) => {
+  console.log(payload)
+  return {
+    type: EDIT_CONTACT,
+    payload,
+  };
+};
